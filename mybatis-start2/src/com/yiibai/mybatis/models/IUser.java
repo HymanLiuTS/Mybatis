6b7acdce953319bd21f518eab3bdf3af886b5604
刘志辉 <879651072@qq.com>
2018-03-18 15:34:06 +0800

    02、Mybatis接口注解

--- mybatis-start2/src/com/yiibai/mybatis/models/IUser.java
+++ mybatis-start2/src/com/yiibai/mybatis/models/IUser.java
@@ -3,5 +3,6 @@ package com.yiibai.mybatis.models;
 import org.apache.ibatis.annotations.Select;
 
 public interface IUser {
+	@Select("select * from user where id= #{id}")
 	public MyUser getUserByID(int id);
 }
